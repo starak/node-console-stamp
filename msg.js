088b6027f07e2212b71284cbbff6ce9f99e0e695
Ståle Raknes <stale@raknes.net>
2019-06-09 21:42:40 +0200

    Prepare for 3.0.0 release

---
+++ msg.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+console.group('Attention')
+console.log(chalk.yellow('Console-stamp version 3.0.0 Release Candidate is out. Install by using the tag \'@next\'. NB: Breaking changes.'));
+console.log('For more details goto: https://www.npmjs.com/package/console-stamp/v/next\n');
+console.groupEnd();
